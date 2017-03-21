376d64c14650fa495a7f6574b5b81f8f0456c093
h3x1e <kingz.williams@gmail.com>
2017-03-21 12:10:55 +0000

    T04 - Creating and Deleting Landscapes                                |                                  Added Landscape to project

---
+++ battle_tank_v1/Source/battle_tank_v1/battle_tank_v1.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "battle_tank_v1.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, battle_tank_v1, "battle_tank_v1" );
---
+++ battle_tank_v1/Source/battle_tank_v1/battle_tank_v1GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "battle_tank_v1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE_TANK_V1_API Abattle_tank_v1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
